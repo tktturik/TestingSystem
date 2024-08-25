48830973f42ce2bb49a54b1d53102ab3ff425944
tktturik <85058042+tktturik@users.noreply.github.com>
2024-08-25 16:00:36 +1000

    Добавлены стили

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -27,5 +27,10 @@ namespace WpfApp1
             InitializeComponent();
 
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
